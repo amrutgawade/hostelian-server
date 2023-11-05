c34d382187359711e3e6e73264e33cf533dce80f
amrutgawade <amrutgawade.23@gmail.com>
2023-11-05 15:02:19 +0530

    Initial commit

---
+++ src/main/java/com/hostelian/hostelianserver/HostelianServerApplication.java
@@ -0,0 +1,13 @@
+package com.hostelian.hostelianserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelianServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelianServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hostelian/hostelianserver/HostelianServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostelian.hostelianserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelianServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
